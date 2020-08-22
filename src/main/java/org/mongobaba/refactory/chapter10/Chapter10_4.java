766483858357292ff9c3f62385cfe4e77aed2ffc
mongobaba <chaowei.meng@gmail.com>
2020-08-22 15:44:54 +0800

    添加10.4的重构工作项标记

--- src/main/java/org/mongobaba/refactory/chapter10/Chapter10_4.java
+++ src/main/java/org/mongobaba/refactory/chapter10/Chapter10_4.java
@@ -5,6 +5,7 @@ package org.mongobaba.refactory.chapter10;
  * 将查询函数和修改函数分离
  */
 public class Chapter10_4 {
+    // TODO 将sendAlert从当前方法中拆分出来
     String foundMiscreant(String[] people) {
         for (int i = 0; i < people.length; i++) {
             if (people[i].equals("Don")) {
