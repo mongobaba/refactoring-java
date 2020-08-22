ed99fadb5105fa08a525c82e04c0a8d4b3e19375
mongobaba <chaowei.meng@gmail.com>
2020-08-22 15:35:19 +0800

    添加10.3的重构工作项标记

--- src/main/java/org/mongobaba/refactory/chapter10/Chapter10_3.java
+++ src/main/java/org/mongobaba/refactory/chapter10/Chapter10_3.java
@@ -5,6 +5,7 @@ package org.mongobaba.refactory.chapter10;
  * 移除参数
  */
 public class Chapter10_3 {
+    // TODO 删除不用的areaCode参数
     static String getTelephoneNumber(int areaCode, int number) {
         return "Telephone: " + number;
     }
