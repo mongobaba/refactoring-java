217c33cfa212fa72ab4a0370f8855c4f37164f36
mongobaba <chaowei.meng@gmail.com>
2020-08-22 13:15:36 +0800

    添加8.5的重构工作项标记

--- src/main/java/org/mongobaba/refactory/chapter8/Chapter8_5.java
+++ src/main/java/org/mongobaba/refactory/chapter8/Chapter8_5.java
@@ -5,6 +5,7 @@ package org.mongobaba.refactory.chapter8;
  * 以对象取代数组
  */
 public class Chapter8_5 {
+    // TODO 创建一个类型，代替row数组
     static String[] createRow() {
         String[] row = new String[3];
         row[0] = "Liverpool";
